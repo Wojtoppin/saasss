441ebd5f8666a075d813725d7fcdbddb4bf1f4cf
WOJTOP_PC\48690 <wojtop@interia.pl>
2024-01-26 23:57:25 +0100

    added modifying student data

--- src/AdminPanel/AdminPanel.js
+++ src/AdminPanel/AdminPanel.js
@@ -14,6 +14,14 @@ const AdminPanel = (props) =>{
   const [filteredGroup, setFilteredGroup] = useState("none");
   const [filterId, setFilteredId] = useState("ASC");
   
+  const [editedId, setEditedId] = useState("none");
+  const [editedName, setEditedName] = useState("");
+  const [editedSurname, setEditedSurname] = useState("");
+  const [editedMail, setEditedMail] = useState("");
+  const [editedNumber, setEditedNumber] = useState("");
+  const [editedGroup, setEditedGroup] = useState("none");
+
+  
 
   const getGroups = (functionData) =>{
     let tmpHighestGroup = 0;
@@ -50,7 +58,6 @@ const AdminPanel = (props) =>{
               && student.telefon.toLowerCase().includes(filteredNumber.toLowerCase())
     ))
     setFilteredData(new_students_displayed)
-    console.log(new_students_displayed)
   }
 
   const reverseIdIndexes = () =>{
@@ -58,6 +65,49 @@ const AdminPanel = (props) =>{
     setFilteredId(filterId === "ASC"? "DESC": "ASC")
   }
 
+  const setEditedValues = (edited_name, edited_surname, edited_mail, edited_number) =>{
+    setEditedName(edited_name)
+    setEditedSurname(edited_surname)
+    setEditedMail(edited_mail)
+    setEditedNumber(edited_number)
+  }
+
+  const sendData = async (data_urodzenia, adres, dataDołączenia, poziomUmiejetnosci, uwagi) =>{
+    try {
+      const response = await fetch('https://zienex.pythonanywhere.com/edit_student_data', {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          'id_ucznia': parseInt(editedId,10),
+          'imie': editedName,
+          'nazwisko': editedSurname,
+          'data_urodzenia': data_urodzenia,
+          'adres_ucznia': adres,
+          'telefon': editedNumber,
+          'email': editedMail,
+          'id_grupy': editedGroup,
+          'data_dolaczenia': dataDołączenia,
+          'poziom_umiejetnosci':poziomUmiejetnosci,
+          'uwagi':uwagi
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Request failed');
+      }else{
+        fetchData();
+      }
+
+      const result = await response.json();
+      console.log('Successfully patched data:', result);
+    } catch (error) {
+      console.error('Error patching data:', error.message);
+    }
+    
+
+  }
 
   useEffect(() => {
     fetchData();
@@ -65,11 +115,6 @@ const AdminPanel = (props) =>{
   
   useEffect(() => {
     filterStudents();
-    console.log({"filteredName": filteredName,
-      "filteredSurname": filteredSurname,
-        "filteredGroup": filteredGroup,
-        "filteredMail": filteredMail,
-          "filteredNumber": filteredNumber})
   }, [filteredName, filteredSurname, filteredMail, filteredNumber, filteredGroup]);
 
   return(
@@ -83,6 +128,7 @@ const AdminPanel = (props) =>{
                       <th>id grupy</th>
                       <th>email ucznia</th>
                       <th>telefon ucznia</th>
+                      <th></th>
                   </tr>
                   <tr>
                       <th>
@@ -100,18 +146,50 @@ const AdminPanel = (props) =>{
                         </Input></th>
                       <th><Input type="text" value={filteredMail} onChange={(event)=>setFilteredMail(event.target.value)}></Input></th>
                       <th><Input type="text" value={filteredNumber} onChange={(event)=>setFilteredNumber(event.target.value)}></Input></th>
+                      <th></th>
                   </tr>
               </thead>
               <tbody>
                   {Array.isArray(filteredData) && filteredData.length !== 0? filteredData.map((student)=>(
-                      <tr key={student.id_ucznia}>
+                      editedId.toString() === student.id_ucznia.toString()?
+                        <tr key={student.id_ucznia}>
                           <td>{student.id_ucznia}</td>
-                          <td>{student.imie}</td>
-                          <td>{student.nazwisko}</td>
-                          <td>{student.id_grupy}</td>
-                          <td>{student.email}</td>
-                          <td>{student.telefon}</td>
-                      </tr>
+                          <td><Input type="text" value={editedName} onChange={(e) => setEditedName(e.target.value)} className="center"></Input></td>
+                          <td><Input type="text" value={editedSurname} onChange={(e) => setEditedSurname(e.target.value)} className="center"></Input></td>
+                          <td><Input type="select" defaultValue={student.id_grupy} onChange={(e) => setEditedGroup(e.target.value)}>
+                          {Array.from({ length: highestGroup }, (_, index) => (
+                            <option key={index + 1} value={index + 1}>
+                              grupa {index + 1}
+                            </option>
+                          ))}
+                          </Input></td>
+                          <td><Input type="text" value={editedMail} onChange={(e) => setEditedMail(e.target.value)} className="center"></Input></td>
+                          <td><Input type="text" value={editedNumber} onChange={(e) => setEditedNumber(e.target.value)} className="center" maxLength={11}></Input></td>
+                          <td>
+
+                            <Button id="editImageButton" onClick={()=>{setEditedId("none"); sendData(student.data_urodzenia, student.adres_ucznia, student.data_dolaczenia, student.poziom_umiejetnosci, student.uwagi)}}>
+                              <img src={process.env.PUBLIC_URL + '/done.png'} id="editImage" alt="done"/>
+                            </Button>{" "}
+
+                            <Button id="editImageButton" onClick={()=>setEditedId("none")}><img src={process.env.PUBLIC_URL + '/cancel.png'} id="editImage" alt="cancel"/></Button>
+                          </td>
+                        </tr>
+                        :
+                        <tr key={student.id_ucznia}>
+                            <td>{student.id_ucznia}</td>
+                            <td>{student.imie}</td>
+                            <td>{student.nazwisko}</td>
+                            <td>{student.id_grupy}</td>
+                            <td>{student.email}</td>
+                            <td>{student.telefon}</td>
+                            <td>
+                              {editedId.toString() === "none"?
+                                <Button id="editImageButton" onClick={()=>{setEditedId(student.id_ucznia);setEditedValues(student.imie, student.nazwisko, student.email, student.telefon)}}><img src={process.env.PUBLIC_URL + '/edit.png'} id="editImage" alt="edit"/></Button>
+                                :
+                                ""}
+                            </td>
+                        </tr>
+                    
                   )):
                     
                       <tr>
