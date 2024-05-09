7545c1fe58d3815f947aa85c935b47d00772bf6e
WOJTOP_PC\48690 <wojtop@interia.pl>
2024-05-09 23:14:12 +0200

    i've sorted the css files

--- src/AdminPanel/Dashboard.js
+++ src/AdminPanel/Dashboard.js
@@ -1,29 +1,74 @@
-import React from 'react'
-import { Container, Button } from "reactstrap";
+import React, { useEffect, useState } from 'react'
+import {Button, Input, Table } from "reactstrap";
 import { Col, Row } from "react-bootstrap";
 import './AdminData.scss'
 
 const Dashboard = () => {
+  const [data, setData] = useState([]);
+  function formatDate(date) {
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+  
+    if (day < 10) {
+      day = `0${day}`;
+    }
+    if (month < 10) {
+      month = `0${month}`;
+    }
+  
+    return `${day}-${month}-${year}`;
+  }
+    const fetchData = async () => {
+      try {
+        const today = new Date();
+        const response = await fetch(`https://zienex.pythonanywhere.com/attendance?date=${formatDate(today)}`);
 
-    // const fetchData = async () => {
-    //   try {
-    //     const response = await fetch('https://zienex.pythonanywhere.com/all-students');
+        if (response.ok) {
+          const responseData = await response.json();
+          // setHeaders(responseData[0]);
+          // setData(responseData.slice(1));
+          setData(responseData["data"])
+        } else {
+          console.error('Failed to fetch data. Status:', response.status);
+        }
+      } catch (error) {
+        console.error('Error during fetch:', error);
+      }
+    };
+  useEffect(()=>{fetchData()},[])
 
-    //     if (response.ok) {
-    //       const responseData = await response.json();
-    //       setHeaders(responseData[0]);
-    //       setData(responseData.slice(1));
-    //     } else {
-    //       console.error('Failed to fetch data. Status:', response.status);
-    //     }
-    //   } catch (error) {
-    //     console.error('Error during fetch:', error);
-    //   }
-    // };
+  const renderTableBody = () =>{
+    return data.map((student, index)=>{
+      return <tr key={`${index}: ${student["Imię"]}`}>
+                <td>{student["Imię"]}</td>
+                <td>{student["Nazwisko"]}</td>
+                <td>{student["Grupa"]}</td>
+                <td><Input readOnly checked={student["attendance"]} type="checkbox" style={{ width: "30%", height: `30px` }} /></td>
+              </tr>
+    })
+  }
 
   return (
     <Row className="mainData" style={{overflowX:"hidden", borderRadius:"10px", height:"95vh"}}>
+        <Table className="dataTable center" size="sm" hover responsive style={{marginBottom:"2px", padding:"0px"}}>
+          <thead>
+          <tr>
+              <td colSpan={4} className="datePicker">Imię</td>
+            </tr>
+            <tr>
+              <td>Imię</td>
+              <td>Nazwisko</td>
+              <td>Grupa</td>
+              <td>obecność</td>
+            </tr>
+          </thead>
+          <tbody>
+            {renderTableBody()}
             
+          </tbody>
+
+        </Table>
     </Row>
   )
 }
